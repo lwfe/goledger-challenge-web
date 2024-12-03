07e2380cfa80fe94f3f5e213202b630ad9d09e91
lwfe <leonardolwfe@gmail.com>
2024-12-02 22:43:39 -0300

    feat: add filterAlbums function to album model and commom types

--- src/models/album.ts
+++ src/models/album.ts
@@ -1,9 +1,34 @@
+import { api } from "@/lib/api";
 import { IArtist } from "@/models/artist";
 
 interface IAlbum {
+  "@assetType": string;
+  "@key": string;
+  "@lastTouchBy": string;
+  "@lastTx": string;
+  "@lastUpdated": string;
   name: string;
   artist: IArtist;
   year: number;
 }
 
+const filterAlbums = async (filters: {
+  filters: { [key: keyof IAlbum | string]: string }[];
+}) => {
+  try {
+    const results = await api.post("/query/search", {
+      query: {
+        selector: {
+          "@assetType": "album",
+          ...filters.filters,
+        },
+      },
+    });
+    return results.data.result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export type { IAlbum };
+export { filterAlbums };
