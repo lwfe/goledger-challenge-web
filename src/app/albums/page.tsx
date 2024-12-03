0715f302ab33d13e01bd20cc44b8fc47cae34dba
lwfe <leonardolwfe@gmail.com>
2024-12-02 22:47:20 -0300

    feat: add a simple map function to render albums

--- src/app/albums/page.tsx
+++ src/app/albums/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+import { useAlbums } from "./hook/use-albums";
 import { LayoutHeader, LayoutWrapper } from "@/components/layout";
 
 export default function AlbumsPage() {
+  const { albums } = useAlbums();
+
   return (
     <LayoutWrapper>
       <LayoutHeader
@@ -10,7 +14,13 @@ export default function AlbumsPage() {
         actionTitle="+ Create Album"
       />
 
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      {/* <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" /> */}
+      {albums?.map((album) => (
+        <div key={album.name}>
+          <p>{album.name}</p>
+          <p>{album["@lastUpdated"]}</p>
+        </div>
+      ))}
     </LayoutWrapper>
   );
 }
