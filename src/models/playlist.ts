1fff90eff0c744e1d23f0364cca7be2ea2101616
lwfe <leonardolwfe@gmail.com>
2024-12-02 22:44:06 -0300

    feat: add commom types to playlist model

--- src/models/playlist.ts
+++ src/models/playlist.ts
@@ -1,6 +1,11 @@
 import { ISong } from "./song";
 
 interface IPlaylist {
+  "@assetType": string;
+  "@key": string;
+  "@lastTouchBy": string;
+  "@lastTx": string;
+  "@lastUpdated": string;
   name: string;
   songs: ISong[];
   private: boolean;
