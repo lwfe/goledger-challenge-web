95b49cf95a653c0e00a0913652a9b9fb6d62c6c0
lwfe <leonardolwfe@gmail.com>
2024-12-02 22:47:00 -0300

    fix: rename .env variables because client requests can only use variables with NEXT_PUBLIC prefix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,25 +1,27 @@
 import axios from "axios";
 
-if (!process.env.BASE_URL) {
-  throw new Error("BASE_URL is not defined Please add it to your .env file.");
-}
-
-if (!process.env.BASIC_AUTH_USERNAME) {
+if (!process.env.NEXT_PUBLIC_BASE_URL) {
   throw new Error(
-    "BASIC_AUTH_USERNAME is not defined. Please add it to your .env file."
+    "NEXT_PUBLIC_BASE_URL is not defined Please add it to your .env file."
   );
 }
 
-if (!process.env.BASIC_AUTH_PASSWORD) {
+if (!process.env.NEXT_PUBLIC_BASIC_AUTH_USERNAME) {
   throw new Error(
-    "BASIC_AUTH_PASSWORD is not defined. Please add it to your .env file."
+    "NEXT_PUBLIC_BASIC_AUTH_USERNAME is not defined. Please add it to your .env file."
+  );
+}
+
+if (!process.env.NEXT_PUBLIC_BASIC_AUTH_PASSWORD) {
+  throw new Error(
+    "NEXT_PUBLIC_BASIC_AUTH_PASSWORD is not defined. Please add it to your .env file."
   );
 }
 
 export const api = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   auth: {
-    username: process.env.BASIC_AUTH_USERNAME,
-    password: process.env.BASIC_AUTH_PASSWORD,
+    username: process.env.NEXT_PUBLIC_BASIC_AUTH_USERNAME,
+    password: process.env.NEXT_PUBLIC_BASIC_AUTH_PASSWORD,
   },
 });
