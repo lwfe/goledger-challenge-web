617a3b88cb716adc2473db19d9217c7abee88f1f
lwfe <leonardolwfe@gmail.com>
2024-12-02 22:43:48 -0300

    feat: add commom types to song model

--- src/models/song.ts
+++ src/models/song.ts
@@ -1,6 +1,11 @@
 import { IAlbum } from "@/models/album";
 
 interface ISong {
+  "@assetType": string;
+  "@key": string;
+  "@lastTouchBy": string;
+  "@lastTx": string;
+  "@lastUpdated": string;
   name: string;
   album: IAlbum;
 }
