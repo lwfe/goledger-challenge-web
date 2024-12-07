e059e1f7640290901482fab29b3e8874ce3c215a
lwfe <leonardolwfe@gmail.com>
2024-12-07 10:37:41 -0300

    refactor: remove playlists from album-artwork component

--- src/app/albums/components/album-artwork.tsx
+++ src/app/albums/components/album-artwork.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { PlusCircle } from "lucide-react";
+import { Pencil, Trash2 } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import {
@@ -7,24 +7,9 @@ import {
   ContextMenuContent,
   ContextMenuItem,
   ContextMenuSeparator,
-  ContextMenuSub,
-  ContextMenuSubContent,
-  ContextMenuSubTrigger,
   ContextMenuTrigger,
 } from "@/components/ui/context-menu";
 import { IAlbum } from "@/models/album";
-
-export type Playlist = (typeof playlists)[number];
-
-export const playlists = [
-  "Logic Discography",
-  "Bedtime Beats",
-  "Feeling Happy",
-  "I miss Y2K Pop",
-  "Runtober",
-  "Mellow Days",
-  "Eminem Essentials",
-];
 
 export interface Album {
   name: string;
@@ -66,38 +51,16 @@ export function AlbumArtwork({
         </ContextMenuTrigger>
         <ContextMenuContent className="w-40">
           <ContextMenuItem>Add to Library</ContextMenuItem>
-          <ContextMenuSub>
-            <ContextMenuSubTrigger>Add to Playlist</ContextMenuSubTrigger>
-            <ContextMenuSubContent className="w-48">
-              <ContextMenuItem>
-                <PlusCircle className="mr-2 h-4 w-4" />
-                New Playlist
-              </ContextMenuItem>
-              <ContextMenuSeparator />
-              <div className="h-[200px] overflow-y-auto">
-                {playlists.map((playlist) => (
-                  <ContextMenuItem key={playlist}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="mr-2 h-4 w-4"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 15V6M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5ZM12 12H3M16 6H3M12 18H3" />
-                    </svg>
-                    {playlist}
-                  </ContextMenuItem>
-                ))}
-              </div>
-            </ContextMenuSubContent>
-          </ContextMenuSub>
+
           <ContextMenuSeparator />
-          <ContextMenuItem>Like</ContextMenuItem>
-          <ContextMenuItem>Share</ContextMenuItem>
+          <ContextMenuItem>
+            <Pencil className="mr-2 h-4 w-4" />
+            Edit
+          </ContextMenuItem>
+          <ContextMenuItem className="text-destructive">
+            <Trash2 className="mr-2 h-4 w-4" />
+            Delete
+          </ContextMenuItem>
         </ContextMenuContent>
       </ContextMenu>
       <div className="space-y-1 text-sm">
