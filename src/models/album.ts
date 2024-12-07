c691a4b5df120b887b7f164c7759c202babf2ba2
lwfe <leonardolwfe@gmail.com>
2024-12-07 10:35:23 -0300

    feat: add create and delete functions to album model

--- src/models/album.ts
+++ src/models/album.ts
@@ -1,5 +1,5 @@
 import { api } from "@/lib/api";
-import { IArtist } from "@/models/artist";
+import { IArtist, IArtistAsset } from "@/models/artist";
 
 interface IAlbum {
   "@assetType": string;
@@ -9,6 +9,12 @@ interface IAlbum {
   "@lastUpdated": string;
   name: string;
   artist: IArtist;
+  year: number;
+}
+
+interface IAlbumAsset {
+  name: string;
+  artist: IArtistAsset;
   year: number;
 }
 
@@ -30,5 +36,40 @@ const filterAlbums = async (filters: {
   }
 };
 
-export type { IAlbum };
-export default Object.freeze({ filterAlbums });
+const createAlbum = async (album: IAlbumAsset) => {
+  try {
+    const results = await api.post("/invoke/createAsset", {
+      asset: [
+        {
+          "@assetType": "album",
+          name: album.name,
+          year: album.year,
+          artist: album.artist,
+        },
+      ],
+    });
+    return results.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.error);
+  }
+};
+
+const deleteAlbum = async (album: IAlbum) => {
+  try {
+    const results = await api.post("/invoke/deleteAsset", {
+      key: {
+        "@assetType": "album",
+        name: album.name,
+        artist: {
+          "@key": album.artist["@key"],
+        },
+      },
+    });
+    return results.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.error);
+  }
+};
+
+export type { IAlbum, IAlbumAsset };
+export default Object.freeze({ filterAlbums, createAlbum, deleteAlbum });
